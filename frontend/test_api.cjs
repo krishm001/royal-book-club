61471e559d32daf5c68bd25a57ef18fe14d4779b
Deepika Kumari <deepikakumari@Deepikas-Laptop.local>
2026-08-27 22:50:25 +0530

    chore: remove test scripts

--- frontend/test_api.cjs
+++
@@ -1,9 +0,0 @@
-const axios = require('axios');
-(async () => {
-  try {
-    const res = await axios.get('http://localhost:8080/api/v1/books/0440296005');
-    console.log(res.data);
-  } catch (e) {
-    console.error(e.message);
-  }
-})();
